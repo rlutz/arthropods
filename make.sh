396acdb262821843a51f3147597ab6bcdbd53841
Roland Lutz <rlutz@hedmen.org>
2015-03-25 22:52:52 +0100

    Fix make.sh

--- make.sh
+++ make.sh
@@ -3,9 +3,9 @@
 for pin_count in 6 16
 do	./arthropods.py --straight --style=thickened SIP${pin_count} $pin_count \
 	  > footprints/SIP${pin_count}.fp
-	./arthropods.py --bended -d300 --style=dent DIP${pin_count} $pin_count \
+	./arthropods.py --u-shaped -d300 --style=dent DIP${pin_count} $pin_count \
 	  > footprints/DIP${pin_count}.fp
-	./arthropods.py --bended -d600 --style=dent DIP${pin_count}_wide $pin_count \
+	./arthropods.py --u-shaped -d600 --style=dent DIP${pin_count}_wide $pin_count \
 	  > footprints/DIPwide${pin_count}.fp
 	./arthropods.py --straight --style=thickened 1x${pin_count}PIN $pin_count \
 	  > footprints/1xPIN${pin_count}.fp
