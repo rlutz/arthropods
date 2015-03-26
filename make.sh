55d4627ec56e96e3bb4b4970d2cc40f3a8be51f9
Roland Lutz <rlutz@hedmen.org>
2015-03-26 19:52:34 +0100

    Add AB and BA shapes

--- make.sh
+++ make.sh
@@ -11,6 +11,10 @@ do	./arthropods -s thickened SIP${pin_count} $pin_count \
 	  > footprints/1xPIN${pin_count}.fp
 	./arthropods -Z -s boxed 2x${pin_count}PIN $[$pin_count * 2] \
 	  > footprints/2xPIN${pin_count}.fp
+	./arthropods -A -s boxed 2x${pin_count}PIN_AB $[$pin_count * 2] \
+	  > footprints/2xPIN${pin_count}_AB.fp
+	./arthropods -B -s boxed 2x${pin_count}PIN_BA $[$pin_count * 2] \
+	  > footprints/2xPIN${pin_count}_BA.fp
 	./arthropods -Z -s wsl-arrow 2x${pin_count}PIN_WSL $[$pin_count * 2] \
 	  > footprints/WSL${pin_count}.fp
 	./arthropods -Z -s wsl-dent 2x${pin_count}PIN_WSL $[$pin_count * 2] \
