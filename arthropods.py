ea84e43ab9707fc05eb1cf5fcf5f2cbf0629acc4
Roland Lutz <rlutz@hedmen.org>
2015-03-26 18:53:24 +0100

    Move utility functions to arthropods.py

--- arthropods.py
+++ arthropods.py
@@ -17,11 +17,6 @@
 # Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
 import getopt, sys
-from fpgen import *
-
-STYLE_THICKENED, STYLE_DENT, STYLE_BOXED, \
-    STYLE_WSL_ARROW, STYLE_WSL_DENT, STYLE_PSL = xrange(6)
-SHAPE_STRAIGHT, SHAPE_ZIGZAG, SHAPE_USHAPED = xrange(3)
 
 pin_thickness = 1.5 # mm
 pin_clearance = 1. # mm
@@ -31,6 +26,104 @@ pin_drill = 1. # mm
 pad_thickness = 1.492 # mm
 pad_clearance = 1. # mm
 pad_mask = 1.59 # mm
+
+# Formatting mm/mil lengths
+
+def format_number(length, unit):
+    if isinstance(length, str):
+        if '.' in length:
+            length = float(length)
+        else:
+            length = int(length)
+    if isinstance(length, int):
+        if length == 0:
+            return "0.0000"
+        return "%d.00%s" % (length, unit)
+    if isinstance(length, float):
+        if length == 0.:
+            return "0.0000"
+        if unit == 'mm':
+            return "%.4f%s" % (length, unit)
+        if unit == 'mil':
+            return "%.2f%s" % (length, unit)
+    raise ValueError
+
+def format(length):
+    if isinstance(length, int) or isinstance(length, float):
+        return format_number(length, 'mm')
+    if isinstance(length, str):
+        if length.endswith("mm"):
+            return format_number(length[:-2], length[-2:])
+        if length.endswith("mil"):
+            return format_number(length[:-3], length[-3:])
+    raise ValueError
+
+# Output
+
+def start(flags, desc, name, value, mark_x, mark_y,
+          text_x, text_y, text_dir, text_scale, text_flags):
+    if isinstance(flags, list):
+        flags = ','.join(flags)
+    elif flags is None:
+        flags = ""
+    if name is None:
+        name = desc
+    if value is None:
+        value = desc
+    if isinstance(text_flags, list):
+        text_flags = ','.join(text_flags)
+    elif text_flags is None:
+        text_flags = ""
+    print 'Element["%s" "%s" "%s" "%s" %s %s %s %s %s %s "%s"]' % (
+        flags, desc, name, value,
+        format(mark_x), format(mark_y), format(text_x), format(text_y),
+        text_dir, text_scale, text_flags)
+    print '('
+
+def pin(x, y, thickness, clearance, mask, drill,
+        name, number = None, flags = None):
+    if number is None:
+        number = name
+    if isinstance(flags, list):
+        flags = ','.join(flags)
+    elif flags is None:
+        flags = ""
+    print "\tPin[%s %s %s %s %s %s \"%s\" \"%s\" \"%s\"]" % (
+        format(x), format(y),
+        format(thickness), format(clearance), format(mask), format(drill),
+        name, number, flags)
+
+def pad(x0, y0, x1, y1, thickness, clearance, mask,
+        name, number = None, flags = None):
+    if number is None:
+        number = name
+    if isinstance(flags, list):
+        flags = ','.join(flags)
+    elif flags is None:
+        flags = ""
+    print "\tPad[%s %s %s %s %s %s %s \"%s\" \"%s\" \"%s\"]" % (
+        format(x0), format(y0), format(x1), format(y1),
+        format(thickness), format(clearance), format(mask),
+        name, number, flags)
+
+def line(x0, y0, x1, y1, thickness):
+    print "\tElementLine [%s %s %s %s %s]" % (
+        format(x0), format(y0), format(x1), format(y1), format(thickness))
+
+def arc(x, y, xradius, yradius, start_angle, delta_angle, thickness):
+    print "\tElementArc [%s %s %s %s %d %d %s]" % (
+        format(x), format(y), format(xradius), format(yradius),
+        start_angle, delta_angle, format(thickness))
+
+def end():
+    print
+    print "\t)"
+
+# Parameter handling
+
+STYLE_THICKENED, STYLE_DENT, STYLE_BOXED, \
+    STYLE_WSL_ARROW, STYLE_WSL_DENT, STYLE_PSL = xrange(6)
+SHAPE_STRAIGHT, SHAPE_ZIGZAG, SHAPE_USHAPED = xrange(3)
 
 def invalid_arguments(message):
     sys.stderr.write("%s: %s\n" % (sys.argv[0], message))
