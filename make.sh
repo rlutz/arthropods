c34cc7bfd6858646285ad8ff8bb7f53e9633e0a3
Roland Lutz <rlutz@hedmen.org>
2015-05-31 16:34:38 +0200

    Pass component name as option

--- make.sh
+++ make.sh
@@ -2,36 +2,36 @@
 
 for pin_count in 6 16
 do	./arthropods -s thickened \
-	    SIP${pin_count} $pin_count \
+	    --name=SIP${pin_count} $pin_count \
 	  > footprints/SIP${pin_count}.fp
 	./arthropods -r 2 -n u-shaped -d 300mil -s dent \
-	    DIP${pin_count} $pin_count \
+	    --name=DIP${pin_count} $pin_count \
 	  > footprints/DIP${pin_count}.fp
 	./arthropods -r 2 -n u-shaped -d 600mil -s dent \
-	    DIP${pin_count}_wide $pin_count \
+	    --name=DIP${pin_count}_wide $pin_count \
 	  > footprints/DIPwide${pin_count}.fp
 	./arthropods -s thickened \
-	    1x${pin_count}PIN $pin_count \
+	    --name=1x${pin_count}PIN $pin_count \
 	  > footprints/1xPIN${pin_count}.fp
 	./arthropods -r 2 -n zigzag -s boxed \
-	    2x${pin_count}PIN $[$pin_count * 2] \
+	    --name=2x${pin_count}PIN $[$pin_count * 2] \
 	  > footprints/2xPIN${pin_count}.fp
 	./arthropods -r 2 -n ab -s boxed \
-	    2x${pin_count}PIN_AB $[$pin_count * 2] \
+	    --name=2x${pin_count}PIN_AB $[$pin_count * 2] \
 	  > footprints/2xPIN${pin_count}_AB.fp
 	./arthropods -r 2 -n ba -s boxed \
-	    2x${pin_count}PIN_BA $[$pin_count * 2] \
+	    --name=2x${pin_count}PIN_BA $[$pin_count * 2] \
 	  > footprints/2xPIN${pin_count}_BA.fp
 	./arthropods -r 2 -n ba -s socket \
-	    2x${pin_count}PINsocket_BA $[$pin_count * 2] \
+	    --name=2x${pin_count}PINsocket_BA $[$pin_count * 2] \
 	  > footprints/2xPIN${pin_count}socket_BA.fp
 	./arthropods -r 2 -n zigzag -s wsl-arrow \
-	    2x${pin_count}PIN_WSL $[$pin_count * 2] \
+	    --name=2x${pin_count}PIN_WSL $[$pin_count * 2] \
 	  > footprints/WSL${pin_count}.fp
 	./arthropods -r 2 -n zigzag -s wsl-dent \
-	    2x${pin_count}PIN_WSL $[$pin_count * 2] \
+	    --name=2x${pin_count}PIN_WSL $[$pin_count * 2] \
 	  > footprints/WSLalt${pin_count}.fp
 	./arthropods -r 2 -n zigzag -s psl \
-	    2x${pin_count}PIN_PSL $[$pin_count * 2] \
+	    --name=2x${pin_count}PIN_PSL $[$pin_count * 2] \
 	  > footprints/PSL${pin_count}.fp
 done
