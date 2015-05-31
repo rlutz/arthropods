92488dbfed11875375d1a8ce5b84b1b30de6691f
Roland Lutz <rlutz@hedmen.org>
2015-05-31 22:40:41 +0200

    Implement variable substitution

--- make.sh
+++ make.sh
@@ -1,48 +1,26 @@
 #!/bin/bash
 
 for pin_count in 6 16
-do	./arthropods \
-	    --name=SIP${pin_count} -P ${pin_count} \
-	    -v SIP config \
+do	./arthropods -v SIP -P ${pin_count} config \
 	  > footprints/SIP${pin_count}.fp
-	./arthropods \
-	    --name=DIP${pin_count} -P ${pin_count} \
-	    -v DIP config \
+	./arthropods -v DIP -P ${pin_count} config \
 	  > footprints/DIP${pin_count}.fp
-	./arthropods \
-	    --name=DIP${pin_count}_wide -P ${pin_count} \
-	    -v DIPwide config \
+	./arthropods -v DIPwide -P ${pin_count} config \
 	  > footprints/DIPwide${pin_count}.fp
-	./arthropods \
-	    --name=1x${pin_count}PIN -p ${pin_count} \
-	    -v 1xXPIN config \
+	./arthropods -v 1xXPIN -p ${pin_count} config \
 	  > footprints/1xPIN${pin_count}.fp
-	./arthropods \
-	    --name=2x${pin_count}PIN -p ${pin_count} \
-	    -v 2xXPIN config \
+	./arthropods -v 2xXPIN -p ${pin_count} config \
 	  > footprints/2xPIN${pin_count}.fp
-	./arthropods \
-	    --name=2x${pin_count}PIN_AB -p ${pin_count} \
-	    -v 2xXPIN_AB config \
+	./arthropods -v 2xXPIN_AB -p ${pin_count} config \
 	  > footprints/2xPIN${pin_count}_AB.fp
-	./arthropods \
-	    --name=2x${pin_count}PIN_BA -p ${pin_count} \
-	    -v 2xXPIN_BA config \
+	./arthropods -v 2xXPIN_BA -p ${pin_count} config \
 	  > footprints/2xPIN${pin_count}_BA.fp
-	./arthropods \
-	    --name=2x${pin_count}PINsocket_BA -p ${pin_count} \
-	    -v 2xXPINsocket_BA config \
+	./arthropods -v 2xXPINsocket_BA -p ${pin_count} config \
 	  > footprints/2xPIN${pin_count}socket_BA.fp
-	./arthropods \
-	    --name=2x${pin_count}PIN_WSL -p ${pin_count} \
-	    -v 2xXPIN_WSL config \
+	./arthropods -v 2xXPIN_WSL -p ${pin_count} config \
 	  > footprints/WSL${pin_count}.fp
-	./arthropods \
-	    --name=2x${pin_count}PIN_WSL -p ${pin_count} \
-	    -v 2xXPIN_WSLalt config \
+	./arthropods -v 2xXPIN_WSLalt -p ${pin_count} config \
 	  > footprints/WSLalt${pin_count}.fp
-	./arthropods \
-	    --name=2x${pin_count}PIN_PSL -p ${pin_count} \
-	    -v 2xXPIN_PSL config \
+	./arthropods -v 2xXPIN_PSL -p ${pin_count} config \
 	  > footprints/PSL${pin_count}.fp
 done
