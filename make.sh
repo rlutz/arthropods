ed8ed65e7f068cc9020b0de779771218f898c42f
Roland Lutz <rlutz@hedmen.org>
2015-06-01 15:01:45 +0200

    Split configuration file

--- make.sh
+++ make.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 for pin_count in 6 16
-do	./arthropods -v SIP -P ${pin_count} config
-	./arthropods -v DIP -P ${pin_count} config
-	./arthropods -v DIP_wide -P ${pin_count} config
-	./arthropods -v 1xXPIN -p ${pin_count} config
-	./arthropods -v 2xXPIN -p ${pin_count} config
-	./arthropods -v 2xXPIN_AB -p ${pin_count} config
-	./arthropods -v 2xXPIN_BA -p ${pin_count} config
-	./arthropods -v 2xXPINsocket_BA -p ${pin_count} config
-	./arthropods -v 2xXPIN_WSL -p ${pin_count} config
-	./arthropods -v 2xXPIN_WSLalt -p ${pin_count} config
-	./arthropods -v 2xXPIN_PSL -p ${pin_count} config
+do	./arthropods -v SIP -P ${pin_count} sip-dip
+	./arthropods -v DIP -P ${pin_count} sip-dip
+	./arthropods -v DIP_wide -P ${pin_count} sip-dip
+	./arthropods -v 1xXPIN -p ${pin_count} connectors
+	./arthropods -v 2xXPIN -p ${pin_count} connectors
+	./arthropods -v 2xXPIN_AB -p ${pin_count} connectors
+	./arthropods -v 2xXPIN_BA -p ${pin_count} connectors
+	./arthropods -v 2xXPINsocket_BA -p ${pin_count} connectors
+	./arthropods -v 2xXPIN_WSL -p ${pin_count} connectors
+	./arthropods -v 2xXPIN_WSLalt -p ${pin_count} connectors
+	./arthropods -v 2xXPIN_PSL -p ${pin_count} connectors
 done
