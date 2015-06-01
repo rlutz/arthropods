52f682b5020a8d03c038442c8875ad3043999f28
Roland Lutz <rlutz@hedmen.org>
2015-06-01 16:52:16 +0200

    Make socket style row-agnostic

--- make.sh
+++ make.sh
@@ -5,6 +5,7 @@ do	./arthropods -v SIP -P ${pin_count} sip-dip
 	./arthropods -v DIP -P ${pin_count} sip-dip
 	./arthropods -v DIP_wide -P ${pin_count} sip-dip
 	./arthropods -v 1xXPIN -p ${pin_count} connectors
+	./arthropods -v 1xXPINsocket -p ${pin_count} connectors
 	./arthropods -v 2xXPIN -p ${pin_count} connectors
 	./arthropods -v 2xXPIN_AB -p ${pin_count} connectors
 	./arthropods -v 2xXPIN_BA -p ${pin_count} connectors
